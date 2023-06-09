93e85f842718547977619f60eb9e4c64588c2094
EkaterinaPashina <k377at@mail.ru>
2023-06-09 12:59:20 +0500

    added func getLastWordLength

---
+++ js.js
@@ -0,0 +1,11 @@
+getLastWordLength = (str) => {
+    const strArr = str.trim().split(' ');
+    const lastSymbol = strArr.at(-1);
+    return lastSymbol.length;
+};
+
+export default getLastWordLength;
+
+console.log(getLastWordLength(''));
+console.log(getLastWordLength('man in BlacK'));
+console.log(getLastWordLength('hello, world!  '));
